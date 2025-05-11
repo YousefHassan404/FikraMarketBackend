82fb5e2a2988a4078bbe0e469214ea0b7ff4750a
Youssef Hassan <145775122+YousefHassan404@users.noreply.github.com>
2025-05-11 05:41:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 // MongoDB connection
-const MONGODB_URI = 'mongodb://18.234.166.80:27017/FikraMarket';
+const MONGODB_URI = 'mongodb://23.21.2.8:27017/FikraMarket';
 
 mongoose.connect(MONGODB_URI, {
     auth: {
